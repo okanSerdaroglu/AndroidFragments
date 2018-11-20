c901f2abaab64743a39a767c60561b0fcc330e8b
okanSerdaroglu <okanserdaroglu2@gmail.com>
2018-11-20 14:15:48 +0300

    life cycle explanations

--- FragmentLifeCycle/app/src/main/java/com/example/okanserdaroglu/fragmentlifecycle/LifeCycleFragment.java
+++ FragmentLifeCycle/app/src/main/java/com/example/okanserdaroglu/fragmentlifecycle/LifeCycleFragment.java
@@ -17,12 +17,16 @@ public class LifeCycleFragment extends Fragment {
     public void onAttach(Context context) {
         super.onAttach(context);
         Log.e(LOG,getString(R.string.fragmentOnAttach));
+        /** the fragment has been attached to activity
+         *  provides the reference to the context of activity
+         */
     }
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.e(LOG,getString(R.string.fragmentOnCreateFragment));
+        /** initialize essential components */
     }
 
     @Nullable
@@ -31,53 +35,75 @@ public class LifeCycleFragment extends Fragment {
             savedInstanceState) {
         Log.e(LOG,getString(R.string.fragmentOnCreateView));
         return inflater.inflate(R.layout.fragmment_life_cycle,container,false);
+        /** fragment draws user interface
+         *  return
+         *  view : fragment layout root view
+         *  null : fragment has no ui
+         */
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         Log.e(LOG,getString(R.string.fragmentOnActivityCreated));
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        Log.e(LOG, getString(R.string.fragmentOnResume));
+        /** called when activity has been created */
     }
 
     @Override
     public void onStart() {
         super.onStart();
         Log.e(LOG,getString(R.string.fragmentOnStart));
+        /** fragment gets visible */
     }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.e(LOG, getString(R.string.fragmentOnResume));
+        /** fragment is running state */
+    }
+
 
     @Override
     public void onPause() {
         super.onPause();
         Log.e(LOG, getString(R.string.fragmentOnPause));
+        /** indication of user leave fragment
+         *  fragment is still visible
+         *  save the state of fragment
+         */
     }
 
     @Override
     public void onStop() {
         super.onStop();
         Log.e(LOG, getString(R.string.fragmentOnStop));
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        Log.e(LOG, getString(R.string.fragmentOnDestroy));
+        /** fragment is not visible
+         *  either host activity has been stopped or the fragment has been removed and added to backstack
+         */
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         Log.e(LOG, getString(R.string.fragmentOnDestroyView));
+        /** opposite to onCreateView
+         *  the view hierarchy of the fragment is removed
+         */
     }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.e(LOG, getString(R.string.fragmentOnDestroy));
+        /** final cleanup of Fragments state */
+    }
+
 
     @Override
     public void onDetach() {
         super.onDetach();
         Log.e(LOG, getString(R.string.fragmentOnDetach));
+        /** fragment has been disassociated from its hosting activity */
     }
 }
--- FragmentLifeCycle/app/src/main/java/com/example/okanserdaroglu/fragmentlifecycle/MainActivity.java
+++ FragmentLifeCycle/app/src/main/java/com/example/okanserdaroglu/fragmentlifecycle/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.okanserdaroglu.fragmentlifecycle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,35 +16,66 @@ public class MainActivity extends AppCompatActivity {
         getSupportFragmentManager().beginTransaction().
                 add(R.id.container,new LifeCycleFragment(),"lifeCycleFragment").commit();
         Log.e(LOG,getString(R.string.activityOnCreate));
+        /** must be implemented
+         *  call setContentView
+         *  initialize display layout
+         *  initialize variables
+         *  start backgroud thread
+         */
     }
 
     @Override
     protected void onStart() {
         super.onStart();
         Log.e(LOG,getString(R.string.activityOnStart));
+        /** register broadcast receivers
+         *  reinitialize states
+         */
+
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         Log.e(LOG,getString(R.string.activityOnResume));
+        /** commit changes
+         *  save variables
+         *  restore variables
+         *  restart animations
+         */
     }
 
     @Override
     protected void onPause() {
         super.onPause();
         Log.e(LOG,getString(R.string.activityOnPause));
+        /** commit changes
+         *  save variables
+         *  save preferences
+         *  stop animations
+         *  unregister broadcastReciever
+         *
+         */
     }
 
     @Override
     protected void onStop() {
         super.onStop();
         Log.e(LOG,getString(R.string.activityOnStop));
+        /** close server connections
+         *  free resources
+         *  unregister broadcast receiver
+         */
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
         Log.e(LOG,getString(R.string.activityOnDestroy));
+        /** close database
+         *  release remaining resources
+         *  stop background thread
+         */
     }
+
 }
